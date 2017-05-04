5b3a9d2d7fc739cf38b9af6289a968ecb1868062
DavidRGomez'git config --global user.email gomez_inc <gomez_inc@ymail.com>
2017-05-03 19:15:54 -0500

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,27 +7,29 @@ var Burger = require("../models/burger.js");
 // Router paths here https://expressjs.com/en/guide/routing.html
 
 router.get("/", (req, res) => {
-  Burger.all(function(data) {
-    let burgers = {
-      hitlist: [],
-      devoured: []
-    };
-    for (i in data) {
-      if (data[i].devoured === 1) {
-        burgers.devoured.push(data[i]);
-      } else {
-        burgers.hitlist.push(data[i]);
-      }
-    }
-    res.render("index", burgers);
-  });
-});
+            Burger.all(function(data) {
+              let burgers = {
+                hitlist: [],
+                devoured: []
+              };
+                for (i in data) {
+                  if (data[i].devoured === 1) {
+                    burgers.devoured.push(data[i]);
+                  } else {
+                    burgers.hitlist.push(data[i]);
+                  }
+                }
+              res.render("index", burgers);
+            });
+          });
 
-router.post("/", (req, res) => {
-  Burger.add(req.body.burgerName, req.body.restaurant, function() {
-    res.redirect("/");
+            router.post("/", (req, res) => {
+              Burger.add(req.body.burgerName, req.body.restaurant, function() {
+                res.redirect("/");
+              });
   });
-});
+
+
 
 router.put("/", function(req, res) {
   Burger.update(req.body.id, function() {
